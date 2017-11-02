17c940701122c2a6f6d7c8c6a1427e5ef1c1a17c
= <=>
2017-11-02 14:14:55 +0000

    Model and Controler for Post has been created successfully

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -7,15 +7,41 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
+
+    //List all post
+    public function  index()
+    {
+        $post = Post::all();
+    }
+
+     //Create a New Post
     public  function createPost(Request $request)
     {
         $post = Post::create($request->all());
          return response()->json($post);
     }
 
-
+   //Updating the Post
     public function updatePost(Request $request, $id)
     {
         $post= Post::find($id);
+        $post->title  = $request->input('title');
+        $post->body  = $request->input('body');
+        $post->views  = $request->input('view');
+
+        $post->save();
+
+        return response()->json($post);
+
+    }
+
+    //Deleting the Post
+
+    public function  deletePost($id)
+    {
+        $post = Post::find($id);
+        $post->delete();
+
+        return response()->json('Removed Successfully');
     }
 }
